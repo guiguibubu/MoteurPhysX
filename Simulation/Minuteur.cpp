7a762654d0c5f0b1f053b2dfff5f2964ee4de8a8
guiguibubu <guibu44@hotmail.fr>
2018-11-08 13:05:08 -0500

    Update Minuteur

--- Simulation/Minuteur.cpp
+++ Simulation/Minuteur.cpp
@@ -1,6 +1,29 @@
 #include "Minuteur.h"
 
-unsigned int Minuteur::getTempsRestant() {
+std::chrono::time_point<std::chrono::high_resolution_clock> Minuteur::now() {
+   return std::chrono::high_resolution_clock::now();
+}
+
+Minuteur::duree_t Minuteur::dureeHours(time_point_t _instantAvant, time_point_t _instantApres) {
+   return std::chrono::duration_cast<std::chrono::hours>(_instantApres - _instantAvant).count();
+}
+Minuteur::duree_t Minuteur::dureeMinutes(time_point_t _instantAvant, time_point_t _instantApres) {
+   return std::chrono::duration_cast<std::chrono::minutes>(_instantApres - _instantAvant).count();
+}
+Minuteur::duree_t Minuteur::dureeSeconds(time_point_t _instantAvant, time_point_t _instantApres) {
+   return std::chrono::duration_cast<std::chrono::seconds>(_instantApres - _instantAvant).count();
+}
+Minuteur::duree_t Minuteur::dureeMilliseconds(time_point_t _instantAvant, time_point_t _instantApres) {
+   return std::chrono::duration_cast<std::chrono::milliseconds>(_instantApres - _instantAvant).count();
+}
+Minuteur::duree_t Minuteur::dureeMicroseconds(time_point_t _instantAvant, time_point_t _instantApres) {
+   return std::chrono::duration_cast<std::chrono::microseconds>(_instantApres - _instantAvant).count();
+}
+Minuteur::duree_t Minuteur::dureeNanoseconds(time_point_t _instantAvant, time_point_t _instantApres){
+   return std::chrono::duration_cast<std::chrono::nanoseconds>(_instantApres - _instantAvant).count();
+}
+
+Minuteur::duree_t Minuteur::getTempsRestant() {
    return timeToReech - dT;
 }
 bool Minuteur::isFinished() {
@@ -13,20 +36,20 @@ bool Minuteur::isConfigured() {
    return timeToReech != 0;
 }
 void Minuteur::start() {
-    oldTime = std::chrono::high_resolution_clock::now();
+    oldTime = now();
     finished = false;
     started = true;
 }
 
 void Minuteur::refresh() {
    if (!finished && started) {
-      dT = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - oldTime).count();
+      dT = dureeSeconds(oldTime, now());
       finished = dT == timeToReech;
       started = !finished;
       timeToReech = (finished) ? 0 : timeToReech;
    }
 }
 
-void Minuteur::setDecompte(unsigned int timeToReech) {
-   this->timeToReech = timeToReech;
+void Minuteur::setDecompte(duree_t _timeToReech) {
+   this->timeToReech = _timeToReech;
 }
--- Simulation/Minuteur.h
+++ Simulation/Minuteur.h
@@ -4,21 +4,32 @@
 #define MINUTEUR_H
 
 class Minuteur {
+   using duree_t = unsigned int;
+   using time_point_t = std::chrono::high_resolution_clock::time_point;
+public:
+   static time_point_t now();
+
+   static duree_t dureeHours(time_point_t _instantAvant, time_point_t _instantApres);
+   static duree_t dureeMinutes(time_point_t _instantAvant, time_point_t _instantApres);
+   static duree_t dureeSeconds(time_point_t _instantAvant, time_point_t _instantApres);
+   static duree_t dureeMilliseconds(time_point_t _instantAvant, time_point_t _instantApres);
+   static duree_t dureeMicroseconds(time_point_t _instantAvant, time_point_t _instantApres);
+   static duree_t dureeNanoseconds(time_point_t _instantAvant, time_point_t _instantApres);
 
 private:
-   std::chrono::time_point<std::chrono::high_resolution_clock> oldTime;
-   unsigned int timeToReech;
-   unsigned short dT;
+   time_point_t oldTime;
+   duree_t timeToReech;
+   duree_t dT;
    bool finished;
    bool started;
 
 public:
-   Minuteur() : Minuteur(unsigned int{}) {}
-   Minuteur(unsigned int timeToReech) : timeToReech{ timeToReech }, dT{}, finished{}, oldTime{}, started{} {}
+   Minuteur() : Minuteur(duree_t{}) {}
+   Minuteur(duree_t _timeToReech) : timeToReech{ _timeToReech }, dT{}, finished{}, oldTime{}, started{} {}
    ~Minuteur() = default;
 
-   unsigned int getTempsRestant();
-   void setDecompte(unsigned int timeToReech);
+   duree_t getTempsRestant();
+   void setDecompte(duree_t _timeToReech);
    bool isFinished();
    bool isStarted();
    bool isConfigured();
